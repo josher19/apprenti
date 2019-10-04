1f03d234d0c9a10ff79d7406603641fe4712333d
Josh Weinstein <jweinstein@cbtnuggets.com>
2019-10-04 11:41:56 -0700

    Tests still failing but closer

--- infer_schema.js
+++ infer_schema.js
@@ -1,1 +1,20 @@
-module.exports = thing => thing;
+const { Type } = require('@avro/types');
+
+// TODO: get unit tests to pass, add more tests
+const infer_fields = fields => fields;
+
+/**
+ * Infer AVRO Schema from data
+ * @param {Array} data
+ * @param {Object} hints about possible types: { name: type }
+ */
+function infer_schema(data, hints={}) {
+    // TODO: when will the next line break or get incorrect results?
+    const schema = Type.forValue(data[0]);
+    return {
+        type: 'record',
+        fields: infer_fields(schema.fields, hints)
+    };
+}
+
+module.exports = infer_schema;
